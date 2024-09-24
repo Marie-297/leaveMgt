87f9420f59926c02f8cc0bb3e6e28ad178e8d0ee
Michael Adonu <79144537+MichaelAdonu@users.noreply.github.com>
2024-09-24 15:26:12 +0000

    Edit done

--- app/(dashboard)/dashboard/credits/BalancesTable.tsx
+++ app/(dashboard)/dashboard/credits/BalancesTable.tsx
@@ -1,110 +1,110 @@
-"use client";
+'use client';
 
-import React from "react";
-import TableWrapper from "@/components/common/TableWrapper";
+import React from 'react';
+import TableWrapper from '@/components/common/TableWrapper';
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Balances } from "@prisma/client";
-import EditBalances from "./EditBalances";
-import { Badge } from "@/components/ui/badge";
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '@/components/ui/table';
+import { Balances } from '@prisma/client';
+import EditBalances from './EditBalances';
+import { Badge } from '@/components/ui/badge';
 
 type BalanceRowType = {
-  title: string;
-  values: string[];
+	title: string;
+	values: string[];
 };
 
 const BalanceRow = ({ title, values }: BalanceRowType) => (
-  <TableHead className="text-center font-extrabold text-gray-400 border-none">
-    {title}
-    <TableRow>
-      {values.map((value, index) => (
-        <TableHead key={index} className="border-none">
-          {value}
-        </TableHead>
-      ))}
-    </TableRow>
-  </TableHead>
+	<TableHead className='text-center font-extrabold text-gray-400 border-none'>
+		{title}
+		<TableRow>
+			{values.map((value, index) => (
+				<TableHead key={index} className='border-none'>
+					{value}
+				</TableHead>
+			))}
+		</TableRow>
+	</TableHead>
 );
 
 type BalanceProps = {
-  balances: Balances[]
-}
+	balances: Balances[];
+};
 
-const BalancesTable = ({balances}: BalanceProps) => {
+const BalancesTable = ({ balances }: BalanceProps) => {
+	const balanceCategories = [
+		{ title: 'ANNUAL', values: ['Credit', 'Used', 'Available'] },
+		{ title: 'CASUAL', values: ['Credit', 'Used', 'Available'] },
+		{ title: 'SICK', values: ['Credit', 'Used', 'Available'] },
+		{ title: 'MATERNITY', values: ['Credit', 'Used', 'Available'] },
+		{ title: 'PATERNITY', values: ['Credit', 'Used', 'Available'] },
+		{ title: 'STUDY', values: ['Credit', 'Used', 'Available'] },
+		{ title: 'UNPAID', values: ['Used'] },
+	];
 
+	const renderTableCells = (bal: CellTypes) => {
+		const categories = [
+			'annual',
+			'casual',
+			'sick',
+			'maternity',
+			'paternity',
+			'study',
+			'unpaid',
+		];
 
-  const balanceCategories = [
-    { title: "ANNUAL", values: ["Credit", "Used", "Available"] },
-    { title: "CASUAL", values: ["Credit", "Used", "Available"] },
-    { title: "SICK", values: ["Credit", "Used", "Available"] },
-    { title: "MATERNITY", values: ["Credit", "Used", "Available"] },
-    { title: "PATERNITY", values: ["Credit", "Used", "Available"] },
-    { title: "STUDY", values: ["Credit", "Used", "Available"] },
-    { title: "UNPAID", values: ["Used"] },
-  ];
+		return categories.map((category) => (
+			<TableCell key={category} className='font-bold'>
+				<TableRow>
+					{['Credit', 'Used', 'Available'].map((type, index) => (
+						<TableCell key={index} className={`w-[100px]`}>
+							{bal[`${category}${type}`]}
+						</TableCell>
+					))}
+				</TableRow>
+			</TableCell>
+		));
+	};
 
-  const renderTableCells = (bal: CellTypes) => {
-    const categories = [ 
-      "annual",
-      "casual",
-      "sick",
-      "maternity",
-      "paternity",
-      "study",
-      "unpaid",
-    ];
-
-    return categories.map((category) => (
-      <TableCell key={category} className="font-bold">
-        <TableRow>
-          {["Credit", "Used", "Available"].map((type, index) => (
-            <TableCell key={index} className={`w-[100px]`}>
-              {bal[`${category}${type}`]}
-            </TableCell>
-          ))}
-        </TableRow>
-      </TableCell>
-    ));
-  };
-
-  return (
-    <TableWrapper title="All User Leave Remaining">
-      <Table className="font-bold">
-        <TableHeader>
-          <TableRow className="font-extrabold ">
-            <TableHead className="text-center">Edit</TableHead>
-            <TableHead className="text-center">User</TableHead>
-            <TableHead className="text-center">Year</TableHead>
-            {balanceCategories.map((category, index) => (
-              <BalanceRow
-                key={index}
-                title={category.title}
-                values={category.values}
-              />
-            ))}
-          </TableRow>
-        </TableHeader>
-        <TableBody className="whitespace-nowrap text-center">
-          {balances?.map((bal) => (
-            <TableRow key={bal.id}>
-              <TableCell>
-                <EditBalances balance={bal}/>
-              </TableCell>
-              <TableCell>{bal.name}</TableCell>
-              <TableCell><Badge>{bal.year} </Badge> </TableCell>
-              {renderTableCells(bal)}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableWrapper>
-  );
+	return (
+		<TableWrapper buttonA={''} buttonB={''} title='All User Leave Remaining'>
+			<Table className='font-bold'>
+				<TableHeader>
+					<TableRow className='font-extrabold '>
+						<TableHead className='text-center'>Edit</TableHead>
+						<TableHead className='text-center'>User</TableHead>
+						<TableHead className='text-center'>Year</TableHead>
+						{balanceCategories.map((category, index) => (
+							<BalanceRow
+								key={index}
+								title={category.title}
+								values={category.values}
+							/>
+						))}
+					</TableRow>
+				</TableHeader>
+				<TableBody className='whitespace-nowrap text-center'>
+					{balances?.map((bal) => (
+						<TableRow key={bal.id}>
+							<TableCell>
+								<EditBalances balance={bal} />
+							</TableCell>
+							<TableCell>{bal.name}</TableCell>
+							<TableCell>
+								<Badge>{bal.year} </Badge>{' '}
+							</TableCell>
+							{renderTableCells(bal)}
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableWrapper>
+	);
 };
 
 export default BalancesTable;
--- components/common/Notificationbell.tsx
+++ components/common/Notificationbell.tsx
@@ -1,32 +1,38 @@
-"use client"
+'use client';
 import { IoNotificationsOutline } from 'react-icons/io5';
 import { useState } from 'react';
 import NotificationList from './NotificationList';
 
 const NotificationBell = ({ userId }: { userId: string }) => {
-  const [isClicked, setIsClicked] = useState(false);
-  const [unreadCount, setUnreadCount] = useState<number>(0);
-  const handleClick = () => {
-    setIsClicked(!isClicked); 
-  };
-  return (
-    <div className="relative">
-      <button title="notification(s)" onClick={handleClick} className="p-2 bg-transparent">
-        <IoNotificationsOutline className={`w-6 h-6 ${isClicked ? 'text-gray-700' : 'text-red-950'}`} />
-        {unreadCount > 0 && (
-          <span className="absolute top-0 right-0 bg-red-500 text-white rounded-full text-xs px-1">
-            {unreadCount}
-          </span>
-        )}
-      </button>
+	const [isClicked, setIsClicked] = useState(false);
+	const [unreadCount, setUnreadCount] = useState<number>(0);
+	const handleClick = () => {
+		setIsClicked(!isClicked);
+	};
+	return (
+		<div className='relative'>
+			<button
+				title='notification(s)'
+				onClick={handleClick}
+				className='p-2 bg-transparent dark:bg-gray-400 rounded-full'
+			>
+				<IoNotificationsOutline
+					className={`w-6 h-6 ${isClicked ? 'text-gray-700' : 'text-red-950'}`}
+				/>
+				{unreadCount > 0 && (
+					<span className='absolute top-0 right-0 bg-red-500 text-white rounded-full text-xs px-1'>
+						{unreadCount}
+					</span>
+				)}
+			</button>
 
-    {isClicked && (
-      <div className="absolute z-50 right-0 top-10 w-96 bg-white shadow-lg p-4 rounded-lg">
-        <NotificationList userId={userId} onMarkAsRead={setUnreadCount} />
-      </div>
-    )}
-  </div>
-  );
+			{isClicked && (
+				<div className='absolute z-50 right-0 top-10 w-96 bg-white shadow-lg p-4 rounded-lg'>
+					<NotificationList userId={userId} onMarkAsRead={setUnreadCount} />
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default NotificationBell;
