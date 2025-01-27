3263d31b603c8215a8ab0748b6ce31ae601eabf5
Mariama Seidu <153280802+Marie-297@users.noreply.github.com>
2025-01-27 20:26:08 +0000

    event api done

--- app/api/event/route.ts
+++ app/api/event/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: NextRequest) {
         startDate,
         title,
         description,
-        userEmail: loggedInUser.email,
+        userEmail: loggedInUser.email || "",
       },
     });
 
