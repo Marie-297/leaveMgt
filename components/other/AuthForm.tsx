89c03693e8c701d73abba1fe9fb63823f688c358
Mariama Seidu <153280802+Marie-297@users.noreply.github.com>
2025-01-28 23:58:12 +0000

    donee

--- components/other/AuthForm.tsx
+++ components/other/AuthForm.tsx
@@ -26,14 +26,8 @@ export function AuthForm() {
 			alert('Please select a role before continuing');
 			return;
 		}
-
-		const callbackUrl =
-    process.env.NODE_ENV === 'production'
-      ? `https://leave-mgt-nine.vercel.app//dashboard?role=${selectedRole}`
-      : `http://localhost:3000/dashboard?role=${selectedRole}`;
-
 		signIn(providerId, {
-			callbackUrl
+			callbackUrl: `/dashboard?role=${selectedRole}`,
 		}).catch((error) => {
 			console.error('Sign-in error:', error);
 			alert('Sign-in failed: You cannot be signed in with this role');
